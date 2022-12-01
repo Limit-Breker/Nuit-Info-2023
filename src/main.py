25e4bdbb4ec682d789bc51f8c632a0b8a9000a16
Sofia Gribanova <grisoffa@gmail.com>
2022-12-01 22:30:13 +0100

    ui

--- src/main.py
+++ src/main.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-
-
+@app.route("/contact.html/")
+def contact():
+    return render_template("contact.html")
 
 app.run(host="0.0.0.0", debug=True)
