058c9af1d1ba37195770aa0b9620116d65bd03a4
Segaelmax23 <116631926+Segaelmax23@users.noreply.github.com>
2022-12-01 22:06:41 +0100

    dsf

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 
 
 app = Flask(__name__)
@@ -6,7 +6,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "hello"
+    return render_template("index.html")
+
 
 
 app.run(host="0.0.0.0", debug=True)
