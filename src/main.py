6b9ba259b3ea3aac9a53669d5b1edce744cc3a2e
Sofia Gribanova <grisoffa@gmail.com>
2022-12-01 22:33:24 +0100

    testrelierpages

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-@app.route("/contact.html/")
+@app.route("/contact.html")
 def contact():
     return render_template("contact.html")
 
