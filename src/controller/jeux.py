992514d171cf4c15b66fbac65a2e5661695d0c42
Sofia Gribanova <grisoffa@gmail.com>
2023-12-07 20:16:09 +0100

    feat: base du truth game pour test

--- src/controller/jeux.py
+++ src/controller/jeux.py
@@ -18,6 +18,9 @@ def index():
 @jeux.route("/earth-survival", methods=['GET', 'POST'])
 def earth_survival():
     return render_template("jeux/earth-survival.html")
+@jeux.route("/truth-game", methods=['GET', 'POST'])
+def truth_game():
+    return render_template("jeux/truth-game.html")
 
 
 
