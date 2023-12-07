3e835f6e2e6e5b665d6760669c11461b45b34446
Gamingdy <christgik12@gmail.com>
2023-12-07 17:25:56 +0100

    feat: déplacement de tout les fichier dans src

--- custom_paquets/converter.py
+++
@@ -1,8 +0,0 @@
-def convertToDict(posts):
-    postList = []
-    if str(type(posts)) == "<class 'sqlalchemy.engine.row.Row'>":
-        postList = posts._asdict()
-    else:
-        for post in posts:
-            postList.append(post._asdict())
-    return postList
