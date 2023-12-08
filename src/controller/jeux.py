6af3da89cb1569a5561db0fd4d167d09c643ba84
Gamingdy <christgik12@gmail.com>
2023-12-08 03:53:39 +0100

    feat : fini partie technique vrai faux

--- src/controller/jeux.py
+++ src/controller/jeux.py
@@ -1,53 +1,47 @@
-from flask import Blueprint, render_template,url_for,redirect
-
 from custom_paquets.decorateur import theme_required
+from flask import Blueprint, render_template
 
-jeux = Blueprint("jeux", __name__, url_prefix="/jeux")
-'''
+jeux = Blueprint("jeux", __name__, url_prefix = "/jeux")
+"""
 Possibilité d'ajouter un prefix à l'url :
 
 communs = Blueprint("communs", __name__, url_prefix="/communs")
 
 De cette manière toutes les urls associées a ce controller commenceront par /communs/...
-'''
+"""
 
 
-@jeux.route("/", methods=['GET', 'POST'])
+@jeux.route("/", methods = ["GET", "POST"])
 @theme_required
 def index():
     return render_template("jeux/index.html")
 
 
-@jeux.route("/earth-survival", methods=['GET', 'POST'])
+@jeux.route("/earth-survival", methods = ["GET", "POST"])
 @theme_required
 def earth_survival():
     return render_template("jeux/earth-survival.html")
 
-@jeux.route("/truth-game/<id>", methods=['GET', 'POST'])
-def truth_game_id(id):
-    
-    return render_template("jeux/truth-game.html",id=id)
 
-@jeux.route("/truth-game")
-def truth_game():
-    return redirect(url_for("jeux.truth_game_id",id=1))
+@jeux.route("/truth-game/", methods = ["GET"])
+@theme_required
+def truth_game_id():
+    return render_template("jeux/truth-game.html")
 
-@jeux.route("/quizz", methods=['GET', 'POST'])
+
+@jeux.route("/quizz", methods = ["GET", "POST"])
 @theme_required
 def quizz():
     return render_template("jeux/quizz.html")
 
 
-@jeux.route("/memory", methods=['GET', 'POST'])
+@jeux.route("/memory", methods = ["GET", "POST"])
 @theme_required
 def memory():
     return render_template("jeux/memory.html")
 
-@jeux.route("/solution", methods=['GET', 'POST'])
+
+@jeux.route("/solution", methods = ["GET", "POST"])
 @theme_required
 def solution():
     return render_template("jeux/solution.html")
-
-
-
-
--- src/static/js/truth-game.js
+++ src/static/js/truth-game.js
@@ -1,42 +1,61 @@
 const xhr = new XMLHttpRequest();
+challengeCounter = 1
+score = 0
 
-function getCardData(id){
-    
-    xhr.open("GET", `/api/truth-game/${id}`);
+function getCardData() {
+
+    xhr.open("GET", `/api/truth-game/${challengeCounter}`);
     xhr.send();
     xhr.responseType = "json";
     xhr.onload = () => {
         data = xhr.response;
-        var titre=document.querySelector(".titre_carte h2");
-        var description=document.querySelector(".description_carte p");
-        titre.innerHTML=data.titre;
-        description.innerHTML=data.description;
-    
-        var titre_explication=document.querySelector(".titre_carte_explication h2");
-        var description_explication=document.querySelector(".description_carte_explication p");
-        titre_explication.innerHTML=data.titre;
+        var titre = document.querySelector(".titre_carte h2");
+        var description = document.querySelector(".description_carte p");
+        titre.innerHTML = data.titre;
+        description.innerHTML = data.description;
+
+        var titre_explication = document.querySelector(".titre_carte_explication h2");
+        var description_explication = document.querySelector(".description_carte_explication p");
+        titre_explication.innerHTML = data.titre;
         description_explication.innerHTML = data.explication;
         initializeCard()
     };
 }
 
-function initializeCard(){
+function initializeCard() {
     carteContenu = document.getElementsByClassName("flip-card-inner")[0];
-    carteContenu.classList.remove("rotate-card")
+    carteContenu.classList.remove("rotate-card");
+    carteContenu.style.backgroundColor = "rgba(217, 217, 217, 0.50)";
+
+    bouton = document.getElementsByClassName("bouton_question_suivante")[0];
+    bouton.style.display = "none";
+
+    let current = document.getElementById("current_question")
+    current.innerHTML = challengeCounter
+
+    let scoreElement = document.getElementById("current_score")
+    scoreElement.innerHTML = score
+
 }
+
+function nextCard() {
+    challengeCounter++
+    getCardData()
+}
+
 
 function flipProfile(buttonName) {
     console.log(buttonName)
     console.log(data)
-    let card = document.getElementsByClassName("flip-card")[0];
     carteContenu.classList.add("rotate-card");
-    if (buttonName==data.response){
-        carteContenu.style.backgroundColor="rgba(82, 210, 79, 0.50)";
-    }else{
-        carteContenu.style.backgroundColor="rgba(181, 19, 19, 0.50)";
+    if (buttonName == data.response) {
+        carteContenu.style.backgroundColor = "rgba(82, 210, 79, 0.50)";
+        score++;
+    } else {
+        carteContenu.style.backgroundColor = "rgba(181, 19, 19, 0.50)";
     }
     let text = ' <input class="bouton bouton_question_suivante" type="button" value=""/>'
-    let bouton = document.createElement('input');
-   
+    bouton.style.display = "block";
+
     console.dir(card);
 }
